112aaba8515b27ef08a106117feac5b5bcbac328
gloomykuriozity <gloomykuriozity@gmail.com>
2023-09-29 22:45:28 +0200

    Init done - main created

---
+++ src/radar_pkg/src/main.cpp
@@ -0,0 +1,15 @@
+/****************************************
+PROGRAM NAME - main
+PROGRAMMER - Kuriozity
+LANGUAGE - C++
+SYSTEM - Windows 11
+CREATION DATE - 29/09/2023
+NOTE - None :)
+*****************************************/
+
+#include <iostream>
+#include <stdlib.h>
+
+int main(){
+    return 0;
+}
