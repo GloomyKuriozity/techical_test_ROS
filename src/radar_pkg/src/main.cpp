9d883a9eb8111d7b5f525dbb4d9f0f44037cd029
gloomykuriozity <gloomykuriozity@gmail.com>
2023-09-30 01:52:20 +0200

    Error check added - Cycling issue

--- src/radar_pkg/src/main.cpp
+++ src/radar_pkg/src/main.cpp
@@ -11,15 +11,33 @@ NOTE - None :)
 #include <fstream>
 #include "functions.h"
 
+bool fileExists(const std::string& fileName) {
+    std::ifstream file(fileName);
+    return file.good();
+}
 
-// To run this code you need to write main < /path/to/inputX.txt
-int main(){
-    //Retreives radar visibility range in inputX.txt
-    //Retreives number of obstacle in inputX.txt
+// To run this code you need to write main /path/to/inputX.txt
+// Pass inputX in argument to know if it exist or not
+int main(int argc, char* argv[]){
+
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_file_name>" << std::endl;
+        return 1;
+    }
+
+    std::string inputFileName = argv[1];
     int visibility;
     int numObstacles;
-    std::cin >> visibility;
-    std::cin >> numObstacles;
+
+    if (!fileExists(inputFileName)) {
+        std::cerr << "Error: Input file '" << inputFileName << "' does not exist." << std::endl;
+        return 1;
+    }
+
+    std::ifstream inputFile(inputFileName);
+
+    inputFile >> visibility;
+    inputFile >> numObstacles;
 
     processObstacle(visibility, numObstacles);
 
