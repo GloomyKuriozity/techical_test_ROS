c4520970912e7e111b0082cfe3a611eacc66abfc
gloomykuriozity <gloomykuriozity@gmail.com>
2023-09-29 23:26:53 +0200

    obstacle struct created X,Y,index - NP

--- src/radar_pkg/src/main.cpp
+++ src/radar_pkg/src/main.cpp
@@ -10,6 +10,20 @@ NOTE - None :)
 #include <iostream>
 #include <stdlib.h>
 
+struct Obstacle {
+/*
+    Group data from Obstacle -detected in inputX.txt file-
+    
+    Args:
+        x (int): X coordinate of obstacle
+        y (int): Y coordinate of obstacle
+        index (int): index number of obstacle in input file
+*/
+    int index;
+    int x;
+    int y;
+};
+
 int main(){
     return 0;
 }
