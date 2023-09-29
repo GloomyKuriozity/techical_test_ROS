67965e6bb158b18b9306bde241c3d1d0fa6e8aa3
gloomykuriozity <gloomykuriozity@gmail.com>
2023-09-30 00:39:58 +0200

    Sort obstacle by angle - NP

--- src/radar_pkg/src/main.cpp
+++ src/radar_pkg/src/main.cpp
@@ -7,23 +7,47 @@ CREATION DATE - 29/09/2023
 NOTE - None :)
 *****************************************/
 
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include <iostream>
-#include <stdlib.h>
+#include <algorithm>
 #include <vector>
 
 struct Obstacle {
-/*
-    Group data from Obstacle -detected in inputX.txt file-
+    /*
+        Group data from Obstacle -detected in inputX.txt file-
     
-    Args:
-        x (int): X coordinate of obstacle
-        y (int): Y coordinate of obstacle
-        index (int): index number of obstacle in input file
-*/
+        Args:
+            x (int): X coordinate of obstacle
+            y (int): Y coordinate of obstacle
+            index (int): index number of obstacle in input file
+    */
     int index;
     int x;
     int y;
 };
+
+bool sortByAngle(const Obstacle& a, const Obstacle& b) {
+    /*
+        Particular order of triage not by linear increasing number but circular increasing angle
+    
+        Args:
+            a (Obstacle&): obstacle 1 information
+            b (Obstacle&): obstacle 2 information
+        
+        Returns:
+            angleA (double): obstacle 1 angle
+            angleB (double): obstacle 2 angle
+    */
+    double angleA = std::atan2(a.y, a.x);
+    double angleB = std::atan2(b.y, b.x);
+    
+    //Normalize angles for comparison consistency
+    angleA = (angleA < 0) ? (angleA + 2 * M_PI) : angleA;
+    angleB = (angleB < 0) ? (angleB + 2 * M_PI) : angleB;
+
+    return angleA < angleB;
+}
 
 int main(){
     //Retreives radar visibility range in inputX.txt
@@ -37,7 +61,7 @@ int main(){
     //Every line of coordinates in the file will be processed as obstacle
     std::vector<Obstacle> obstacles;
 
-    //Fill obstacle info and push it the vector listing all obstacles
+    //Fill obstacle info and push it in the vector listing all obstacles
     //BUT only consider obstacle in visibility range
     for (int i = 0; i < numObstacles; ++i) {
         int x, y;
@@ -50,6 +74,11 @@ int main(){
             obstacles.push_back(obstacle);
         }
     }
+
+
+    // Sort obstacles based on angle.
+    // sortByAngle impose the order by increasing angle
+    std::sort(obstacles.begin(), obstacles.end(), sortByAngle);
 
     return 0;
 }
