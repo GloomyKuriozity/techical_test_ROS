daf6f5a6ba89f34de58747f3708877d700e479ef
gloomykuriozity <gloomykuriozity@gmail.com>
2023-09-30 23:45:48 +0200

    Read input file - NP

--- rl_ws/src/rl_pkg/src/main2.cpp
+++ rl_ws/src/rl_pkg/src/main2.cpp
@@ -8,9 +8,18 @@ NOTE - None :)
 *****************************************/
 
 #include <iostream>
-
+#include <vector>
 
 int main() {
-    
+    //Create a vector that will stack up all the points
+    //Create coordinates recipients that will be stack up for every points
+    std::vector<std::pair<double, double>> data;
+    double x, y;
+
+    //Read data from standard input.txt and put it in data vector
+    while (std::cin >> x >> y) {
+        data.emplace_back(x, y);
+    }
+
     return 0;
 }
