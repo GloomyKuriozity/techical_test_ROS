85631473886e4e9a35401db187c1be3f156c2bfe
gloomykuriozity <gloomykuriozity@gmail.com>
2023-09-30 23:31:42 +0200

    Init main file - NP

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -38,6 +38,26 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/ws/techical_test_ROS/build/main2.exe")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/main2.exe" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/main2.exe")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "C:/msys64/mingw64/bin/strip.exe" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/main2.exe")
+    endif()
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/rl_pkg" TYPE EXECUTABLE FILES "C:/ws/techical_test_ROS/build/main2.exe")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/rl_pkg/main2.exe" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/rl_pkg/main2.exe")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "C:/msys64/mingw64/bin/strip.exe" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/rl_pkg/main2.exe")
+    endif()
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "C:/ws/techical_test_ROS/build/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/rl_pkg")
 endif()
 
---
+++ rl_ws/src/rl_pkg/src/main2.cpp
@@ -0,0 +1,16 @@
+/****************************************
+PROGRAM NAME - main
+PROGRAMMER - Kuriozity
+LANGUAGE - C++
+SYSTEM - Windows 11
+CREATION DATE - 30/09/2023
+NOTE - None :)
+*****************************************/
+
+#include <iostream>
+
+
+int main() {
+    
+    return 0;
+}
